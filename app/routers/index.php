154dd0b1b25af800733809de00871ee328c7df90
Greta911 <grema20012000@yahoo.it>
2026-04-16 15:15:40 +0200

    Route pages.home

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,1 +1,9 @@
 <?php
+
+//ROUTE PAR DEFAUT
+//PATTERN: /
+//CTRL: PagesController (composite)
+//ACTION: pages/home
+
+include_once "../app/controllers/pagesController.php";
+\App\Controllers\PagesController\homeAction($conn);
