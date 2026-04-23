08df3216cfe6469ddc5b89ffc44888fd1ff6ee24
Greta911 <grema20012000@yahoo.it>
2026-04-23 13:50:48 +0200

    Section popular Recipes

--- app/controllers/pagesController.php
+++ app/controllers/pagesController.php
@@ -10,6 +10,7 @@ function homeAction(PDO $conn)
 {
     include_once '../app/models/recipesModel.php';
     $randomRecipe = RecipesModel\findOneByRand($conn);
+    $popularRecipes = RecipesModel\findAllPopulars($conn);
     global $title, $content;
     $title = "Home";
     ob_start();
